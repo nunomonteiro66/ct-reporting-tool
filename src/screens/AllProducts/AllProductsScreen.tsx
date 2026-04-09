d1e1f69fe2d55b2745714c7d9067a40c0ba759a7
Nuno Monteiro <nunomonteiro_6@hotmail.com>
2026-04-09 14:19:53 +0100

    fix: removal of testing fetch

--- src/screens/AllProducts/AllProductsScreen.tsx
+++ src/screens/AllProducts/AllProductsScreen.tsx
@@ -59,14 +59,14 @@ const AllProducts = () => {
   useEffect(() => {
     const load = async () => {
       const productTypes = await getAllProductTypes();
-      //const rawData = await getAllProducts();
-      const rawData = (
+      const rawData = await getAllProducts();
+      /* const rawData = (
         await getProducts(0, 10, [
           '160001057B0100',
           '172105070C0100',
           '17210521B0100',
         ])
-      ).data.results;
+      ).data.results; */
       const languages = await getAllLanguagesCodes();
       setRawData(rawData);
       setProductTypes(productTypes);
